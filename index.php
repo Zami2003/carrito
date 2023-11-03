a62cc92d73f874941ce4922248bf2fd94adc9de3
Maricelahuaman <131275155+Zami2003@users.noreply.github.com>
2023-11-03 11:46:14 -0500

    g

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>zamira</title>
 </head>
 <body>
-    <h1>hola</h1>
+    <h1>Bienvenido a mi página</h1>
 </body>
 </html>
